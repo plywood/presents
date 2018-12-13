46d2f9728e9bd6b2b34d50fa0e462f3ea71ee7b5
pavel <ploven@gmail.com>
2018-12-13 01:01:33 -0500

    initial commit of present_6 with gyro and OLED

---
+++ present_6/gyro_L3G4200.ino
@@ -0,0 +1,142 @@
+// GYRO code is from here http://forum.arduino.cc/index.php?topic=120111.0
+// GYRO module is AGD8 2216 694CA 
+// Added small 4 pin I2C OLED 
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+// this is for gyro:
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define CTRL_REG5 0x24
+int L3G4200D_Address = 105; //I2C address of the L3G4200D (69 HEX)
+int x;
+int y;
+int z;
+
+
+void setup(){
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("starting up L3G4200D");
+  setupL3G4200D(2000); // Configure L3G4200  - 250, 500 or 2000 deg/sec
+  delay(1500); //wait for the sensor to be ready
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.clearDisplay();
+  
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, gyro!");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+}
+
+void loop(){
+  
+  getGyroValues();  // This will update x, y, and z with new values
+  display.clearDisplay();
+  /*Serial.print("X:");
+  Serial.print(x);
+  Serial.print(" Y:");
+  Serial.print(y);
+  Serial.print(" Z:");
+  Serial.println(z);
+  */
+  
+  display.setCursor(0,0);
+  display.print("X"); 
+  display.setCursor(20,0);
+  display.print(x);
+  display.setCursor(0,20);
+  display.print("Y"); 
+  display.setCursor(20,20);
+  display.print(y);
+  display.setCursor(0,40);
+  display.print("Z"); 
+  display.setCursor(20,40);
+  display.print(z);
+  
+  display.display();
+  
+
+  delay(100); //Just here to slow down the serial to make it more readable
+}
+
+void getGyroValues(){
+
+  byte xMSB = readRegister(L3G4200D_Address, 0x29);
+  byte xLSB = readRegister(L3G4200D_Address, 0x28);
+  x = ((xMSB << 8) | xLSB);
+
+  byte yMSB = readRegister(L3G4200D_Address, 0x2B);
+  byte yLSB = readRegister(L3G4200D_Address, 0x2A);
+  y = ((yMSB << 8) | yLSB);
+
+  byte zMSB = readRegister(L3G4200D_Address, 0x2D);
+  byte zLSB = readRegister(L3G4200D_Address, 0x2C);
+  z = ((zMSB << 8) | zLSB);
+}
+
+int setupL3G4200D(int scale){
+  //From  Jim Lindblom of Sparkfun's code
+
+  // Enable x, y, z and turn off power down:
+  writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
+
+  // If you'd like to adjust/use the HPF, you can edit the line below to configure CTRL_REG2:
+  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000000);
+
+  // Configure CTRL_REG3 to generate data ready interrupt on INT2
+  // No interrupts used on INT1, if you'd like to configure INT1
+  // or INT2 otherwise, consult the datasheet:
+  writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
+
+  // CTRL_REG4 controls the full-scale range, among other things:
+
+  if(scale == 250){
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00000000);
+  }else if(scale == 500){
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00010000);
+  }else{
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00110000);
+  }
+
+  // CTRL_REG5 controls high-pass filtering of outputs, use it
+  // if you'd like:
+  writeRegister(L3G4200D_Address, CTRL_REG5, 0b00000000);
+}
+
+void writeRegister(int deviceAddress, byte address, byte val) {
+    Wire.beginTransmission(deviceAddress); // start transmission to device
+    Wire.write(address);       // send register address
+    Wire.write(val);         // send value to write
+    Wire.endTransmission();     // end transmission
+}
+
+int readRegister(int deviceAddress, byte address){
+
+    int v;
+    Wire.beginTransmission(deviceAddress);
+    Wire.write(address); // register to read
+    Wire.endTransmission();
+
+    Wire.requestFrom(deviceAddress, 1); // read a byte
+
+    while(!Wire.available()) {
+        // waiting
+    }
+
+    v = Wire.read();
+    return v;
+}
